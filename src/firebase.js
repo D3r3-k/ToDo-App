45e6f4f27cc5c402b123754d9148fe2fca800d45
D3r3k <derek16ib@gmail.com>
2023-01-03 18:52:58 -0600

    agregando variables de entorno

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import env from "react-dotenv";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyA_R_Xijy4HP8frTB_04GtAmU9euNoBgFM",
-    authDomain: "levelup-9f9a8.firebaseapp.com",
-    projectId: "levelup-9f9a8",
-    storageBucket: "levelup-9f9a8.appspot.com",
-    messagingSenderId: "565324358562",
-    appId: "1:565324358562:web:5fccf7da5272fb6c74c8fb"
+    apiKey: env.API_KEY,
+    authDomain: env.AUTH_DOMAIN,
+    projectId: env.PROJECT_ID,
+    storageBucket: env.STORAGE_BUCKET,
+    messagingSenderId: env.MESSAGING_SENDER_ID,
+    appId: env.APP_ID
 };
 
 // Initialize Firebase
