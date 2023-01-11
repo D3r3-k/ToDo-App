6d0f622dd2989ccbd46fdd37fa30b0238a791928
D3r3k <derek16ib@gmail.com>
2023-01-11 11:00:07 -0600

    agregando min-heigh al home

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const WebName = "ToDo-App"
   return (
     <>
-      <main className="pb-16 lg:pb-24 min-h-screen">
+      <main className="pb-16 lg:pb-24 min-h-90-screen">
         <AuthProvider>
           <Routes>
             <Route path='/' element={
